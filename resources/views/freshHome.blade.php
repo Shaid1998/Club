c02f26cd22e1bff6413a1281930dde4972ad5ef5
Shaid1998 <shaidur35-2300@diu.edu.bd>
2020-11-28 15:31:19 +0600

    second commit

---
+++ resources/views/freshHome.blade.php
@@ -0,0 +1,339 @@
+
+
+<!DOCKTYPE html>
+
+<html>
+	<head>
+		<title> Our Club Official Website </title>
+		<link rel="stylesheet" href="{{ asset('css/Style.css')}}" />
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"/>
+		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" ></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" ></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.1/js/bootstrap.min.js" ></script>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
+	</head>
+	<body>
+		<!------------------------------------------------------slider--------------------------------------------------------->
+		<div id="slider">
+			<div id="headerslider" class="carousel slide" data-ride="carousel">
+			  <ol class="carousel-indicators">
+				<li data-target="#headerslider" data-slide-to="0" class="active"></li>
+				<li data-target="#headerslider" data-slide-to="1"></li>
+				<li data-target="#headerslider" data-slide-to="2"></li>
+			  </ol>
+			  <div class="carousel-inner">
+				<div class="carousel-item active">
+				  <img src="img/a1.jpg" class="d-block " />
+				  <div class="carousel-caption" >
+				  <h5> Maclaren 620R </h5>
+				  </div>
+				</div>
+				<div class="carousel-item">
+				  <img src="img/386effbd-lamborghini-veneno-.jpg" class="d-block " />
+				  <div class="carousel-caption" >
+				  <h5> Lamborgini Veneno </h5>
+				  </div>
+				</div>
+				<div class="carousel-item">
+				  <img src="img/1200px-Bugatti_Divo,_GIMS_2019,_Le_Grand-Saconnex_(GIMS0029).jpg" class="d-block " />
+				  <div class="carousel-caption" >
+				  <h5> Bugatti Divo </h5>
+				  </div>
+				</div>
+			  </div>
+			  <a class="carousel-control-prev" href="#headerslider" role="button" data-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			  </a>
+			  <a class="carousel-control-next" href="#headerslider" role="button" data-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			  </a>
+			</div>
+		</div>
+		<!------------------------------------------------------About--------------------------------------------------------->
+		<section id="about">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<h2>About Us</h2> 
+						<div class="about-content">Hi, this is mixed type club. this club have car, motorcycle, bicycle. this club gives this equipment rent for money. thia club also have a sports team. the team playes for club.</div>
+						<button type="button" class="btn btn-primary" href="#">Read More>>></button>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!------------------------------------------------------Services--------------------------------------------------------->
+		<section id="services">
+			<div class="container">
+				<h1>Our Services</h1>
+				<div class="row services">
+					<div class="col-md-3 text-center">
+						<div class="icon">
+							<i class="fa fa-car" aria-hidden="true"></i>
+						</div>
+						<h3>Car rent</h3>
+						<p>We have some rent for car. anyone can rent from our club. </p>
+					</div>
+					<div class="col-md-3 text-center">
+						<div class="icon">
+							<i class="fa fa-motorcycle" aria-hidden="true"></i>
+						</div>
+						<h3>Motorcycle rent</h3>
+						<p>We have some rent for motorcycle. anyone can rent from our club. </p>
+					</div>
+					<div class="col-md-3 text-center">
+						<div class="icon">
+							<i class="fa fa-bicycle" aria-hidden="true"></i>
+						</div>
+						<h3>Bicycle rent</h3>
+						<p>We have some rent for bicycle. anyone can rent from our club. </p>
+					</div>
+					<div class="col-md-3 text-center">
+						<div class="icon">
+                        <i class="fa fa-calendar" aria-hidden="true"></i>
+						</div>
+						<h3>Event</h3>
+						<p>We have a sports team.this team playes for club. </p>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!------------------------------------------------------Team--------------------------------------------------------->
+		<section id="team">
+			<div class="container">
+			<h1>Team Member</h1>
+			<div class="row">
+				<div class="col-md-1 profile-pic text-center">
+					<div class="img-box">
+						<img src="img/KFP3-promo-po4.jpg" class="img-responsive"/>
+						<ul>
+							<a href="#"><li><i class="fa fa-facebook" aria-hidden="true"></i></li></a>
+							<a href="#"><li><i class="fa fa-twitter" aria-hidden="true"></i></li></a>
+							<a href="#"><li><i class="fa fa-linkedin" aria-hidden="true"></i></li></a>
+						</ul>
+					</div>
+					<h2>Mr. Po</h2>
+					
+				</div>
+				<div class="col-md-1 profile-pic text-center">
+					<div class="img-box">
+						<img src="img/bob-the-minion-illustration-png-clip-art.png" class="img-responsive"/>
+						<ul>
+							<a href="#"><li><i class="fa fa-facebook" aria-hidden="true"></i></li></a>
+							<a href="#"><li><i class="fa fa-twitter" aria-hidden="true"></i></li></a>
+							<a href="#"><li><i class="fa fa-linkedin" aria-hidden="true"></i></li></a>
+						</ul>
+					</div>
+					<h2>Master Shifu</h2>
+					
+				</div>
+				<div class="col-md-1 profile-pic text-center">
+					<div class="img-box">
+						<img src="img/download (1).jfif" class="img-responsive"/>
+						<ul>
+							<a href="#"><li><i class="fa fa-facebook" aria-hidden="true"></i></li></a>
+							<a href="#"><li><i class="fa fa-twitter" aria-hidden="true"></i></li></a>
+							<a href="#"><li><i class="fa fa-linkedin" aria-hidden="true"></i></li></a>
+						</ul>
+					</div>
+					<h2>master tiger</h2>
+					
+				</div>
+				<div class="col-md-1 profile-pic text-center">
+					<div class="img-box">
+						<img src="img/unnamed.jpg" class="img-responsive"/>
+						<ul>
+							<a href="#"><li><i class="fa fa-facebook" aria-hidden="true"></i></li></a>
+							<a href="#"><li><i class="fa fa-twitter" aria-hidden="true"></i></li></a>
+							<a href="#"><li><i class="fa fa-linkedin" aria-hidden="true"></i></li></a>
+						</ul>
+					</div>
+					<h2>Tai Lung</h2>
+					
+				</div>
+                <div class="col-md-1 profile-pic text-center">
+					<div class="img-box">
+						<img src="img/tom.jpg" class="img-responsive"/>
+						<ul>
+							<a href="#"><li><i class="fa fa-facebook" aria-hidden="true"></i></li></a>
+							<a href="#"><li><i class="fa fa-twitter" aria-hidden="true"></i></li></a>
+							<a href="#"><li><i class="fa fa-linkedin" aria-hidden="true"></i></li></a>
+						</ul>
+					</div>
+					<h2>Mr. TOM</h2>
+					
+				</div>
+                <div class="col-md-1 profile-pic text-center">
+					<div class="img-box">
+						<img src="img/Jerry_Mouse.png" class="img-responsive"/>
+						<ul>
+							<a href="#"><li><i class="fa fa-facebook" aria-hidden="true"></i></li></a>
+							<a href="#"><li><i class="fa fa-twitter" aria-hidden="true"></i></li></a>
+							<a href="#"><li><i class="fa fa-linkedin" aria-hidden="true"></i></li></a>
+						</ul>
+					</div>
+					<h2>Jerry</h2>
+					
+				</div>
+                <div class="col-md-1 profile-pic text-center">
+					<div class="img-box">
+						<img src="img/Butchcat.png" class="img-responsive"/>
+						<ul>
+							<a href="#"><li><i class="fa fa-facebook" aria-hidden="true"></i></li></a>
+							<a href="#"><li><i class="fa fa-twitter" aria-hidden="true"></i></li></a>
+							<a href="#"><li><i class="fa fa-linkedin" aria-hidden="true"></i></li></a>
+						</ul>
+					</div>
+					<h2>Butchcat</h2>
+					
+				</div>
+                <div class="col-md-1 profile-pic text-center">
+					<div class="img-box">
+						<img src="img/Spike.jpg" class="img-responsive"/>
+						<ul>
+							<a href="#"><li><i class="fa fa-facebook" aria-hidden="true"></i></li></a>
+							<a href="#"><li><i class="fa fa-twitter" aria-hidden="true"></i></li></a>
+							<a href="#"><li><i class="fa fa-linkedin" aria-hidden="true"></i></li></a>
+						</ul>
+					</div>
+					<h2>Spike</h2>
+					
+				</div>
+                <div class="col-md-1 profile-pic text-center">
+					<div class="img-box">
+						<img src="img/mrBean.jpg" class="img-responsive"/>
+						<ul>
+							<a href="#"><li><i class="fa fa-facebook" aria-hidden="true"></i></li></a>
+							<a href="#"><li><i class="fa fa-twitter" aria-hidden="true"></i></li></a>
+							<a href="#"><li><i class="fa fa-linkedin" aria-hidden="true"></i></li></a>
+						</ul>
+					</div>
+					<h2>Mr. Bean</h2>
+					
+				</div>
+                <div class="col-md-1 profile-pic text-center">
+					<div class="img-box">
+						<img src="img/brahmanandam.jpeg" class="img-responsive"/>
+						<ul>
+							<a href="#"><li><i class="fa fa-facebook" aria-hidden="true"></i></li></a>
+							<a href="#"><li><i class="fa fa-twitter" aria-hidden="true"></i></li></a>
+							<a href="#"><li><i class="fa fa-linkedin" aria-hidden="true"></i></li></a>
+						</ul>
+					</div>
+					<h2>Brahmanan</h2>
+					
+				</div>
+                <div class="col-md-1 profile-pic text-center">
+					<div class="img-box">
+						<img src="img/motu.jpg" class="img-responsive"/>
+						<ul>
+							<a href="#"><li><i class="fa fa-facebook" aria-hidden="true"></i></li></a>
+							<a href="#"><li><i class="fa fa-twitter" aria-hidden="true"></i></li></a>
+							<a href="#"><li><i class="fa fa-linkedin" aria-hidden="true"></i></li></a>
+						</ul>
+					</div>
+					<h2>Mr. Motu</h2>
+					
+				</div>
+
+			</div>
+			</div>
+		</section>
+		<!------------------------------------------------------promo--------------------------------------------------------->
+		<section id="promo">
+			<div class="container">
+				<p>Join as a Trainer</p>
+				<a href="#" class="btn btn-primary">Join</a>
+			</div>
+		</section>
+		<!------------------------------------------------------price plans--------------------------------------------------------->
+		<section id="price">
+			<div class="container">
+				<h1>Price Plans</h1>
+				<div class="row">
+					<div class="col-md-6">
+						<div class="single-price">
+							<div class="price-head">
+							<h2>Free</h2>
+							<p>$0<span> Unlimited</span></p>
+							</div>
+							<div class="price-content">
+								<ul>
+									
+									<li><i class="fa fa-check-circle" ></i>Member</li>
+									<li><i class="fa fa-times-circle" ></i>Work as a Admin</li>
+								</ul>
+							</div>
+							<div class="price-button">
+								<a class="buy-btn" href="#">Join free</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-6">
+						<div class="single-price">
+							<div class="price-head">
+							<h2>Premium</h2>
+							<p>$100<span> Month</span></p>
+							</div>
+							<div class="price-content">
+								<ul>
+									
+									<li><i class="fa fa-check-circle" ></i>Member</li>
+									<li><i class="fa fa-check-circle" ></i>Work as a Admin</li>
+								</ul>
+							</div>
+							<div class="price-button">
+								<a class="buy-btn" href="#">Buy Now</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+        		<!-----------------------------------------------Contact--------------------------------------------------------->
+		<section id="contact">
+			<div class="container">
+				<h1>Contact Us</h1>
+				<div class="row">
+					<div class="col-md-6">
+						<form class="contact-form">
+							<div class="form-group">
+								<input type="text" class="form-control" placeholder="Your Name" />
+							</div>
+							<div class="form-group">
+								<input type="number" class="form-control" placeholder="Your Phone" />
+							</div>
+							<div class="form-group">
+								<input type="email" class="form-control" placeholder="Your email" />
+							</div>
+							<div class="form-group">
+								<textarea  class="form-control" rows="4" placeholder="Massage"></textarea>
+							</div>
+							<button type="submit" class="btn btn-primary">Send</button>
+						</form>
+					</div>
+					<div class="col-md-6 contact-info">
+						<div class="follow"><b>Address:</b><i class="fa fa-map-marker" ></i>Mirpur road, DHAKA-1205</div>
+						<div class="follow"><b>Phone:</b><i class="fa fa-phone" ></i>+880 1792048762</div>
+						<div class="follow"><b>Email:</b><i class="fa fa-envelope-o" ></i>Offic@ourclub.com</div>
+						<div class="follow"><label><b>Social Media:</b></label>
+							<a href="#"><i class="fa fa-facebook" ></i></a>
+							<a href="#"><i class="fa fa-twitter" ></i></a>
+							<a href="#"><i class="fa fa-linkedin" ></i></a>
+							<a href="#"><i class="fa fa-youtube-play" ></i></a>
+							<a href="#"><i class="fa fa-google-plus" ></i></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!-----------------------------------------------Footer--------------------------------------------------------->
+		<section id="footer">
+			<div class="container text-center">
+				<p>Stay With Us and Take Wishes From <i class="fa fa-heart-o" ></i></p>
+			</div>
+		</section>
+	</body>
+</html>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    @extends('freshHome');
                 </div>
             </div>
         </div>
